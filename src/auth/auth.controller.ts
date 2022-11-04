1e5ac311727d253763d81be8ffbdf121e0911353
chrislemus <icrislemus@gmail.com>
2022-11-04 07:05:56 -0400

    add jwt strategy and enable bearer auth in swagger document

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -7,10 +10,21 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  // @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Req() req) {
-    // return req.user;
+  async login(@Body() user: LoginDto, @Req() req: any) {
     return this.authService.login(req.user);
+  }
+
+  // @UseGuards(LocalAuthGuard)
+  // @Post('login')
+  // async login(@Req() req) {
+  //   return this.authService.login(req.user);
+  // }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiBearerAuth()
+  getProfile(@Req() req) {
+    return req.user;
   }
 }
