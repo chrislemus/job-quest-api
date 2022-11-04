6441b7dcc4cec5bddd97124cf04e86629ddb5549
chrislemus <icrislemus@gmail.com>
2022-11-04 14:07:26 -0400

    import jwt secret using config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { validate } from 'class-validator';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot(), UsersModule],
+  imports: [AuthModule, ConfigModule.forRoot({ validate }), UsersModule],
 })
 export class AppModule {}
