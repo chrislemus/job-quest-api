a95a8492eb2136a068f1c33c626a044a9f9a4124
chrislemus <icrislemus@gmail.com>
2022-11-05 12:48:40 -0400

    add refresh token and createdAt fields to User table

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,8 +2,10 @@ import { User } from '@prisma/client';
 
 export class UserEntity implements User {
   id: number;
+  createdAt: Date;
   email: string;
   firstName: string;
   lastName: string | null;
   password: string;
+  refreshToken: string;
 }
