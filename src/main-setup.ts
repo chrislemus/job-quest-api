6e1a3c70559eaf2023224b3fa404ea16ad438235
chrislemus <icrislemus@gmail.com>
2022-11-22 16:20:28 -0500

    add serverless config boilerplate

---
+++ src/main-setup.ts
@@ -0,0 +1,44 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Page } from './common/pagination/page.dto';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+
+export async function appSetup(): Promise<INestApplication> {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true, // transform payloads based on TS type
+      },
+    }),
+  );
+
+  // apply transform to all responses
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.enableCors({ origin: '*', methods: '*' });
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Quest API')
+    .setDescription('Job Quest API Docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .setExternalDoc('JSON Schema', '/api-json')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    extraModels: [Page],
+  });
+
+  SwaggerModule.setup('api', app, document);
+
+  return app;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,8 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { Page } from './common/pagination/page.dto';
+import { appSetup } from './main-setup';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true, // transform payloads based on TS type
-      },
-    }),
-  );
-
-  // apply transform to all responses
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
-  app.enableCors({ origin: '*', methods: '*' });
-
-  const config = new DocumentBuilder()
-    .setTitle('Job Quest API')
-    .setDescription('Job Quest API Docs')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .setExternalDoc('JSON Schema', '/api-json')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config, {
-    extraModels: [Page],
-  });
-
-  SwaggerModule.setup('api', app, document);
-
+  const app = await appSetup();
   await app.listen(3001);
 }
+
 bootstrap();
