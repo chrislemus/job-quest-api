c9e3bdf6e45e30216bb1f63da9f61e9fcf1d2175
chrislemus <icrislemus@gmail.com>
2022-11-28 20:23:53 -0500

    add auth jsdoc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -23,7 +23,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   /**
-   * User Log In
+   * User Login
    */
   @SkipAuth()
   @UseGuards(LocalAuthGuard)
@@ -33,12 +33,18 @@ export class AuthController {
     return this.authService.localLogin(user);
   }
 
+  /**
+   * User Signup
+   */
   @SkipAuth()
   @Post('signup')
   signup(@Body() user: CreateUserDto): Promise<AuthTokens> {
     return this.authService.signup(user);
   }
 
+  /**
+   * Refresh Auth JWT
+   */
   @SkipAuth()
   @UseGuards(JwtRefreshAuthGuard)
   @Post('refresh')
@@ -48,12 +54,19 @@ export class AuthController {
     return this.authService.refreshJwt(user.id, user.refreshToken);
   }
 
+  /**
+   * User Logout
+   */
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   logout(@GetAuthUser('id') id: number): Promise<boolean> {
     return this.authService.logout(id);
   }
 
+  /**
+   *
+   * Register admin for the first time to activate app
+   */
   @Post('register-admin')
   @HttpCode(HttpStatus.OK)
   async registerAdmin(
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,11 +20,17 @@ export class AuthService {
     private configService: ConfigService,
   ) {}
 
+  /**
+   * User local login.
+   */
   async localLogin(user: AuthUser): Promise<AuthTokens> {
     const tokens = await this.getTokens(user);
     return tokens;
   }
 
+  /**
+   * Refresh auth JWT.
+   */
   async refreshJwt(userId: number, refreshToken: string): Promise<AuthTokens> {
     const user = await this.prisma.user.findUnique({
       where: { id: userId },
@@ -39,12 +45,18 @@ export class AuthService {
     return tokens;
   }
 
+  /**
+   * Default hash encryption.
+   */
   async hashValue(value: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(value, salt);
     return hash;
   }
 
+  /**
+   * Generates and returns auth tokens.
+   */
   async getTokens(user: AuthUser): Promise<AuthTokens> {
     const jwtPayload: any = {
       sub: user.id,
@@ -67,6 +79,9 @@ export class AuthService {
     return { accessToken, refreshToken };
   }
 
+  /**
+   * Sync refresh token with DB for persistence.
+   */
   async syncRefreshToken(userId: number, refreshToken: string): Promise<void> {
     try {
       const hashRt = await this.hashValue(refreshToken);
@@ -80,6 +95,9 @@ export class AuthService {
     }
   }
 
+  /**
+   * Logout user (removes refresh token from DB).
+   */
   async logout(userId: number): Promise<boolean> {
     await this.prisma.user.updateMany({
       where: {
@@ -91,6 +109,9 @@ export class AuthService {
     return true;
   }
 
+  /**
+   * New user signup.
+   */
   async signup(newUserData: CreateUserDto) {
     const password = await this.hashValue(newUserData.password);
 
@@ -137,6 +158,9 @@ export class AuthService {
     return tokens;
   }
 
+  /**
+   * Register user as admin(when app runs in new env for first time and no admin users exist).
+   */
   async registerAdmin(userId: number, adminKey: string): Promise<UserProfile> {
     const existingAdminUser = await this.prisma.user.findFirst({
       where: { role: 'ADMIN' },
